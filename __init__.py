a863e369e4916fb11c21b002808884ec744010bf
yang0 <yang0@126.com>
2024-10-10 18:45:01 +0800

    'aa'

--- __init__.py
+++ __init__.py
@@ -2,8 +2,17 @@ from datetime import datetime
 
 VERSION = "1.0.0"
 GIT_URL = "https://github.com/yang0/plugin4"
-TASK_NAME = "示例任务4"
-TASK_DESCRIPTION = "这是一个示例任务4，用于演示插件结构"
+NAME = "示例任务4"
+DESCRIPTION = "这是一个示例任务4，用于演示插件结构"
 
-def do_task():
-    print(f"执行任务4: {datetime.now()}")
+# 添加参数描述常量
+PARAM_DESCRIPTIONS = {
+    "test_param": {       
+        "label": "测试参数",
+        "description": "这是一个测试参数"
+    },
+}
+
+def do_task(config={}):
+    print(f"执行plugin4: {datetime.now()}")
+    print(f"config['test_param']: {config['test_param']}")
